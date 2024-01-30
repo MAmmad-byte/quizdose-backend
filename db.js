b076ee7962bd943a1e31d0d8135fcbc43d4fcb86
Muhammad Amamd <muhammadammad146@gmail.com>
2024-01-30 17:29:24 +0500

    Dabase String

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const  mongoose  = require("mongoose");
 
 
-module.exports = mongoose.connect("mongodb://127.0.0.1:27017/quiz-dose")
+module.exports = mongoose.connect(process.env.DBstring)
 .then(()=>console.log("Connected to Database..."))
 .catch((err)=>console.log("Database Error:", err))
