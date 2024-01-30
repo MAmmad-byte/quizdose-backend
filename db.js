8145b4c6814f45452bfab4b20420e5e65ad771ca
Muhammad Amamd <muhammadammad146@gmail.com>
2024-01-30 17:46:55 +0500

    Dabase String

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const  mongoose  = require("mongoose");
 
 
-module.exports = mongoose.connect(process.env.DBstring)
+module.exports = mongoose.connect("mongodb+srv://masteram070:merapig@cluster0.qk8gi0i.mongodb.net//quizdose")
 .then(()=>console.log("Connected to Database..."))
 .catch((err)=>console.log("Database Error:", err))
